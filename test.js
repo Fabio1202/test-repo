0ca45943dc2ed619e98880046955b1afab60d085
Fabio <38130618+Fabio1202@users.noreply.github.com>
2025-02-03 22:51:39 +0100

    Update test.js

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ $('document').ready(function() {
 
   let items = document.getElementsByClassName("alert");
   for (var i = 0; i < items.length; i++) {
-    items[i].style.visibility='hidden';
+    items[i].style.display='none';
   }
   
   document.getElementById('passwort_login').onchange = function() {
